84ea4ca1ae32ced65c21a3a3ca5ad1752642a293
Andrei Avram <andrei.avram@gmail.com>
2024-06-18 19:14:46 +0300

    cleanup

--- organizer/settings.py
+++ organizer/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'organizer.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = "user.OrganizerUser"
+# AUTH_USER_MODEL = "user.OrganizerUser"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
